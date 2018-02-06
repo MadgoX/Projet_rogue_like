bc75bf3b487921ef79b200a2e15d97e0fe52c526
lucien bouyeure <l2info004@ic2s114-18.univ-lemans.fr>
2018-02-06 10:31:10 +0100

    Yes

---
+++ create_map.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include"rogue_like.h"
+
+
+void creer_map(char matrice[N][M], const int N, const int M)
+{
+
+	int i, j;
+	char res;
+	
+	for(i=0; i<N; i++){
+		for(j=0; j<M; j++){
+			matrice[i][j]=' ';
+		}
+	}
+
+	i=0;
+	for(j=1; j<M-1; j++){
+		matrice[i][j]='=';
+	}
+	
+	j=0;
+	for(i=1; i<N-1; i++){
+		matrice[i][j]='#';
+	}
+
+	j=M-1;
+	for(i=1; i<N-1; i++){
+		matrice[i][j]='#';
+	}
+
+	i=N-1;
+	for(j=1; j<M-1; j++){
+		matrice[i][j]='=';
+	}
+	
+	i=0;
+	j=0;
+	matrice[i][j]='+';
+
+	i=N-1;
+	j=M-1;
+	matrice[i][j]='+';
+	
+	i=N-1;
+	j=0;
+	matrice[i][j]='+';
+
+	i=0;
+	j=M-1;
+	matrice[i][j]='+';
+
+	matrice=tresor(matrice, N-1, M-1);
+
+	for(i=0; i<N; i++){
+		for(j=0; j<M; j++){
+			res=matrice[i][j];
+			printf("%c",res);
+		}
+		printf("\n");
+	}
+}
