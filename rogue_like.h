a5bc5a8d50f9db6e7c222f9615adbca09680895a
Yan72 <36165247+Yan72@users.noreply.github.com>
2018-02-06 11:08:21 +0100

    Update rogue_like.h

--- rogue_like.h
+++ rogue_like.h
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
+#define N 30
+#define M 50
 
-void creer_map(char matrice[][], const int N, const int M);
+void creer_map(char matrice[N][M]);
 char tresor(char matrice[][], int longueur, int largeur);
