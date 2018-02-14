bc3788dbd20f1fd778d2fc47b982c0d48173d1e8
MadgoX <36188123+MadgoX@users.noreply.github.com>
2018-02-14 16:13:33 +0100

    Update rogue_like.h

--- rogue_like.h
+++ rogue_like.h
@@ -7,3 +7,9 @@
 void creer_map(char matrice[N][M]);
 int tresorx(int longueur);
 int tresory(int largeur);
+void creer_piece1(char matrice[N][M]);
+void creer_piece2(char matrice[N][M]);
+void creer_piece3(char matrice[N][M]);
+void creer_piece21(char matrice[N][M]);
+void placement_tresors(char matrice[N][M]);
+int main(void);
