08f5356a020ff06704dbfcb39849021c3b616603
Romane <36165672+iamcalledr@users.noreply.github.com>
2018-02-18 14:59:45 +0100

    quelques modifications

--- main.c
+++ main.c
@@ -3,27 +3,56 @@
 #include "rogue_like.h"
 
 int main() {
-	//Creation de la map avec toutes les pieces et les tresors
-	char map[N][M];
-
-	//On cree d'abord la matrice et ses bordures
-	creer_map(map);
-
-	//On cree ensuite les pieces
-	creer_piece1(map);
-	creer_piece2(map);
-	creer_piece3(map);
-//	creer_piece(map);
+	int niv;
+	
+	printf("Entrez le numéro du niveau : ");
+	scanf("%i", &niv);
+	
+	if(niv == 1){
+		//Creation de la map avec toutes les pieces et les tresors
+		char map[N][M];
+	
+		//On cree d'abord la matrice et ses bordures
+		creer_map(map);
+	
+		//On cree ensuite les pieces
+		creer_piece1(map);
+		creer_piece2(map);
+		creer_piece3(map);
+	}
+	else if(niv == 2){
+		//Creation de la map avec toutes les pieces et les tresors
+		char map[N+20][M+10];
+		
+		//On cree d'abord la matrice et ses bordures
+		creer_map(map);
+		
+		//On cree ensuite les pieces
+		creer_piece21(map);
+		creer_piece22(map);
+		creer_piece23(map);
+		creer_piece24(map);
+	}
+	else if(niv == 3){
+		//Creation de la map avec toutes les pieces et les tresors
+		char map[N+30][M+10];
+		
+		//On cree d'abord la matrice et ses bordures
+		creer_map(map);
+		
+		//On cree ensuite les pieces
+		creer_piece31(map);
+		creer_piece32(map);
+		creer_piece33(map);
+		creer_piece34(map);
+		creer_piece35(map);
+	}
 
 	//Et enfin on place les tresors
 	placement_tresors(map);
 
 	//Affichage de la map
-	for(int i = 0; i < N; i++){
-		for(int j = 0; j < M; j++){
-			printf("%c",map[i][j]);
-		}
-		printf("\n");
-	}
+	affichage_map(map);
+	
 	return 0;
 }
