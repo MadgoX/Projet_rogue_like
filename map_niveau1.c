0d2c243b0b66f6973db51d771aab5757e7677bda
Romane <36165672+iamcalledr@users.noreply.github.com>
2018-02-18 15:00:23 +0100

    Maps des niveaux

--- map_niveau1.c
+++ map_niveau1.c
@@ -1,147 +1,149 @@
 #include<stdio.h>
-#include"create_map.h"
+#include"rogue_like.h"
 
+/**
+*	Création d'une première pièce, en haut à gauche de la map
+*/
 void creer_piece1(char matrice[N][M]){
 
-	/*Appel de la fonction qui créer le tour de la map*/
-	creer_map(matrice);
-
 	int i, j;
+	int x_haut = 4;
+	int x_bas = 12;
+	int y_gauche = 5;
+	int y_droite = 15;
 	
 	/*Creation d'une piece en haut à gauche de la map*/
-	for(i = 4; i < 12; i++){
-		for(j = 5; j < 15; j++){
+	for(i = x_haut; i < x_bas; i++){
+		for(j = y_gauche; j < y_droite; j++){
 			matrice[i][j] = ' ';
 		}
 	}
 
 	/*Mur de gauche de la pièce*/
-	for(i = 4; i < 12; i++){
-		j = 4;
+	for(i = x_haut; i < x_bas; i++){
+		j = y_gauche-1;
 		matrice[i][j] = '|';
 	}
 
 	/*Mur de droite de la pièce*/
-	for(i =  4; i < 12; i++){
-		j = 15;
+	for(i =  x_haut; i < x_bas; i++){
+		j = y_droite;
 		matrice[i][j] = '|';
 	}
 
 	/*Angles des pièces*/
-	matrice[3][4] = '+';
-	matrice[3][15] = '+';
-	matrice[12][4] = '+';
-	matrice[12][15] = '+';
+	matrice[x_haut-1][x_haut-1] = '+';
+	matrice[x_haut-1][y_droite] = '+';
+	matrice[x_bas][x_haut-1] = '+';
+	matrice[x_bas][y_droite] = '+';
 
 	/*Mur du haut de la pièce*/
-	for(j = 5; j < 15; j++){
-		i = 3;
+	for(j = y_gauche; j < y_droite; j++){
+		i = x_haut-1;
 		matrice[i][j] = '-';
 	}
 
 	/*Mur du bas de la pièce*/
-	for(j = 5; j < 15; j++){
-		i = 12;
+	for(j = y_gauche; j < y_droite; j++){
+		i = x_bas;
 		matrice[i][j] = '-';
 	}
 }
 
+/**
+*	Création d'une seconde pièce, en haut à droite de la map
+*/
 void creer_piece2(char matrice[N][M]){
 
 	int i, j;
+	int x_haut = 5;
+	int x_bas = 13;
+	int y_gauche = 25;
+	int y_droite = 40;
 	
 	/*Creation d'une piece en haut à droite de la map*/
-	for(i = 5; i < 13; i++){
-		for(j = 25; j < 40; j++){
+	for(i = x_haut; i < x_bas; i++){
+		for(j = y_gauche; j < y_droite; j++){
 			matrice[i][j] = ' ';
 		}
 	}
 
 	//Mur de gauche de la pièce
-	for(i = 5; i < 13; i++){
-		j = 24;
+	for(i = x_haut; i < x_bas; i++){
+		j = y_gauche-1;
 		matrice[i][j] = '|';
 	}
 
 	//Mur de droite de la pièce
-	for(i =  5; i < 13; i++){
-		j = 40;
+	for(i =  x_haut; i < x_bas; i++){
+		j = y_droite;
 		matrice[i][j] = '|';
 	}
 
 	//Angles des pièces
-	matrice[4][24] = '+';
-	matrice[4][40] = '+';
-	matrice[13][24] = '+';
-	matrice[13][40] = '+';
+	matrice[x_haut-1][y_gauche-1] = '+';
+	matrice[x_haut-1][y_droite] = '+';
+	matrice[x_bas][y_gauche-1] = '+';
+	matrice[x_bas][y_droite] = '+';
 
 	//Mur du haut de la pièce
-	for(j = 25; j < 40; j++){
-		i = 4;
+	for(j = y_gauche; j < y_droite; j++){
+		i = x_haut-1;
 		matrice[i][j] = '-';
 	}
 
 	//Mur du bas de la pièce
-	for(j = 25; j < 40; j++){
-		i = 13;
+	for(j = y_gauche; j < y_droite; j++){
+		i = x_bas;
 		matrice[i][j] = '-';
 	}
 }
 
+/**
+*	Création d'une troisième pièce, dans le bas de la map
+*/
 void creer_piece3(char matrice[N][M]){
 
 	int i, j;
+	int x_haut = 18;
+	int x_bas = 25;
+	int y_gauche = 10;
+	int y_droite = 44;
 	
-	/*Creation d'une piece en bas de la map*/
-	for(i = 18; i < 25; i++){
-		for(j = 10; j < 44; j++){
+	/*Creation d'une piece en haut à droite de la map*/
+	for(i = x_haut; i < x_bas; i++){
+		for(j = y_gauche; j < y_droite; j++){
 			matrice[i][j] = ' ';
 		}
 	}
 
 	//Mur de gauche de la pièce
-	for(i = 18; i < 25; i++){
-		j = 9;
+	for(i = x_haut; i < x_bas; i++){
+		j = y_gauche-1;
 		matrice[i][j] = '|';
 	}
 
 	//Mur de droite de la pièce
-	for(i =  18; i < 25; i++){
-		j = 44;
+	for(i =  x_haut; i < x_bas; i++){
+		j = y_droite;
 		matrice[i][j] = '|';
 	}
 
 	//Angles des pièces
-	matrice[17][9] = '+';
-	matrice[17][44] = '+';
-	matrice[25][9] = '+';
-	matrice[25][44] = '+';
+	matrice[x_haut-1][y_gauche-1] = '+';
+	matrice[x_haut-1][y_droite] = '+';
+	matrice[x_bas][y_gauche-1] = '+';
+	matrice[x_bas][y_droite] = '+';
 
 	//Mur du haut de la pièce
-	for(j = 10; j < 44; j++){
-		i = 17;
+	for(j = y_gauche; j < y_droite; j++){
+		i = x_haut-1;
 		matrice[i][j] = '-';
 	}
 
 	//Mur du bas de la pièce
-	for(j = 10; j < 44; j++){
-		i = 25;
+	for(j = y_gauche; j < y_droite; j++){
+		i = x_bas;
 		matrice[i][j] = '-';
-	}
-}
-
-int main(){
-	int i, j;
-	char map[N][M];
-	creer_piece1(map);
-	creer_piece2(map);
-	creer_piece3(map);
-
-	//Affichage de la map
-	for(i = 0; i < N; i++){
-		for(j = 0; j < M; j++){
-			printf("%c",map[i][j]);
-		}
-		printf("\n");
 	}
 }
--- map_niveau3.c
+++ map_niveau3.c
@@ -4,13 +4,13 @@
 /**
 *	Création d'une première pièce en haut à gauche de la map
 */
-void creer_piece21(char matrice[N][M]){
+void creer_piece31(char matrice[N][M]){
 
 	int i, j;
 	int x_haut = 5;
-	int x_bas = 15;
-	int y_gauche = 7;
-	int y_droite = 25;
+	int x_bas = 12;
+	int y_gauche = 5;
+	int y_droite = 22;
 	
 	//Creation d'une piece en haut à gauche de la map
 	for(i = x_haut; i < x_bas; i++){
@@ -53,12 +53,12 @@ void creer_piece21(char matrice[N][M]){
 /**
 *	Création d'une pièce, en haut à droite de la map
 */
-void creer_piece22(char matrice[N][M]){
+void creer_piece32(char matrice[N][M]){
 
 	int i, j;
-	int x_haut = 10;
-	int x_bas = 25;
-	int y_gauche = 35;
+	int x_haut = 8;
+	int x_bas = 19;
+	int y_gauche = 30;
 	int y_droite = 55;
 	
 	//Creation d'une piece en haut à droite de la map
@@ -102,13 +102,13 @@ void creer_piece22(char matrice[N][M]){
 /**
 *	Création d'une pièce, sur la gauche de la map
 */
-void creer_piece23(char matrice[N][M]){
+void creer_piece33(char matrice[N][M]){
 
 	int i, j;
-	int x_haut = 22;
-	int x_bas = 32;
-	int y_gauche = 5;
-	int y_droite = 20;
+	int x_haut = 25;
+	int x_bas = 35;
+	int y_gauche = 6;
+	int y_droite = 31;
 	
 	//Creation d'une piece sur la gauche de la map
 	for(i = x_haut; i < x_bas; i++){
@@ -149,17 +149,66 @@ void creer_piece23(char matrice[N][M]){
 }
 
 /**
-*	Création d'une pièce, en bas de la map
+*	Création d'une pièce, en bas à gauche de la map
 */
-void creer_piece24(char matrice[N][M]){
+void creer_piece34(char matrice[N][M]){
 
 	int i, j;
-	int x_haut = 37;
-	int x_bas = 46;
-	int y_gauche = 20;
-	int y_droite = 53;
+	int x_haut = 43;
+	int x_bas = 55;
+	int y_gauche = 8;
+	int y_droite = 26;
 	
-	//Creation d'une piece en bas de la map
+	//Creation d'une piece en bas à gauche de la map
+	for(i = x_haut; i < x_bas; i++){
+		for(j = y_gauche; j < y_droite; j++){
+			matrice[i][j] = ' ';
+		}
+	}
+
+	//Mur de gauche de la pièce
+	for(i = x_haut; i < x_bas; i++){
+		j = y_gauche-1;
+		matrice[i][j] = '|';
+	}
+
+	//Mur de droite de la pièce
+	for(i =  x_haut; i < x_bas; i++){
+		j = y_droite;
+		matrice[i][j] = '|';
+	}
+
+	//Angles des pièces
+	matrice[x_haut-1][y_gauche-1] = '+';
+	matrice[x_haut-1][y_droite] = '+';
+	matrice[x_bas][y_gauche-1] = '+';
+	matrice[x_bas][y_droite] = '+';
+
+	//Mur du haut de la pièce
+	for(j = y_gauche; j < y_droite; j++){
+		i = x_haut-1;
+		matrice[i][j] = '-';
+	}
+
+	//Mur du bas de la pièce
+	for(j = y_gauche; j < y_droite; j++){
+		i = x_bas;
+		matrice[i][j] = '-';
+	}
+}
+
+/**
+*	Création d'une pièce, à droite de la map
+*/
+void creer_piece35(char matrice[N][M]){
+
+	int i, j;
+	int x_haut = 28;
+	int x_bas = 50;
+	int y_gauche = 39;
+	int y_droite = 54;
+	
+	//Creation d'une piece à droite de la map
 	for(i = x_haut; i < x_bas; i++){
 		for(j = y_gauche; j < y_droite; j++){
 			matrice[i][j] = ' ';
