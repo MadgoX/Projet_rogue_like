d0e6832bf3e290c9e69263a063f2ed372e7d2fbe
Romane <36165672+iamcalledr@users.noreply.github.com>
2018-02-19 16:08:35 +0100

    Add files via upload

--- rogue_like.h
+++ rogue_like.h
@@ -1,10 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
+
 #define N 30
 #define M 50
 
-void creer_map(char matrice[N][M]);
+#define A 50
+#define B 60
+
+#define C 60
+
+void creer_map1(char matrice[N][M]);
+void creer_map2(char matrice[A][B]);
+void creer_map3(char matrice[C][C]);
 
 int tresorx(int longueur);
 int tresory(int largeur);
@@ -13,18 +21,20 @@ void creer_piece1(char matrice[N][M]);
 void creer_piece2(char matrice[N][M]);
 void creer_piece3(char matrice[N][M]);
 
-void creer_piece21(char matrice[N][M]);
-void creer_piece22(char matrice[N][M]);
-void creer_piece23(char matrice[N][M]);
-void creer_piece24(char matrice[N][M]);
+void creer_piece21(char matrice[A][B]);
+void creer_piece22(char matrice[A][B]);
+void creer_piece23(char matrice[A][B]);
+void creer_piece24(char matrice[A][B]);
 
-void creer_piece31(char matrice[N][M]);
-void creer_piece32(char matrice[N][M]);
-void creer_piece33(char matrice[N][M]);
-void creer_piece34(char matrice[N][M]);
-void creer_piece35(char matrice[N][M]);
+void creer_piece31(char matrice[C][C]);
+void creer_piece32(char matrice[C][C]);
+void creer_piece33(char matrice[C][C]);
+void creer_piece34(char matrice[C][C]);
+void creer_piece35(char matrice[C][C]);
 
-void affichage_map(char matrice[N][M]);
+void affichage_map1(char matrice[N][M]);
+void affichage_map2(char matrice[A][B]);
+void affichage_map3(char matrice[C][C]);
 
 void placement_tresors(char matrice[N][M]);
 
