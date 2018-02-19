a334b339974033c29336ce9f419a09912cee823b
Romane <36165672+iamcalledr@users.noreply.github.com>
2018-02-19 16:07:37 +0100

    Terminé

--- affichage_map.c
+++ affichage_map.c
@@ -1,13 +1,37 @@
 #include<stdio.h>
 #include"rogue_like.h"
 
-void affichage_map(char matrice[N][M]){
+void affichage_map1(char matrice[N][M]){
 	int i, j;
 	
 	//Affichage de la map
 	for(i = 0; i < N; i++){
 		for(j = 0; j < M; j++){
-			printf("%c",map[i][j]);
+			printf("%c",matrice[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void affichage_map2(char matrice[A][B]){
+	int i, j;
+	
+	//Affichage de la map
+	for(i = 0; i < A; i++){
+		for(j = 0; j < B; j++){
+			printf("%c",matrice[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void affichage_map3(char matrice[C][C]){
+	int i, j;
+	
+	//Affichage de la map
+	for(i = 0; i < C; i++){
+		for(j = 0; j < C; j++){
+			printf("%c",matrice[i][j]);
 		}
 		printf("\n");
 	}
