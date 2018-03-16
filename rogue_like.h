fa67d9e61cfa321ce380cc6653e0a19faa56a125
MadgoX <36188123+MadgoX@users.noreply.github.com>
2018-03-16 11:57:15 +0100

    Update rogue_like.h

--- rogue_like.h
+++ rogue_like.h
@@ -1,44 +1,53 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<unistd.h>
-
 #define N 30
 #define M 50
+#define O 50
+#define P 60
+#define Q 120
 
-#define A 50
-#define B 60
+//niveau 1 : 30/50
+//niveau 2 : 50/60
+//niveau 3 : 50/120
 
-#define C 50
-#define D 120
 
-void creer_map1(char matrice[N][M]);
-void creer_map2(char matrice[A][B]);
-void creer_map3(char matrice[C][D]);
+void creer_map(char matrice[N][M]);
+void creer_map2(char matrice[O][P]);
+void creer_map3(char matrice[M][Q]);
 
 int tresorx(int longueur);
 int tresory(int largeur);
 
+int rand_a_b(int a, int b);
+
+void creer_piece(char matrice[N][M], int x_haut, int y_gauche, int min_larg, int max_larg, int min_haut, int max_haut);
+void creer_piece2(char matrice[O][P], int x_haut, int y_gauche, int min_larg, int max_larg, int min_haut, int max_haut);
+void creer_piece3(char matrice[M][Q], int x_haut, int y_gauche, int min_larg, int max_larg, int min_haut, int max_haut);
+
+/*
 void creer_piece1(char matrice[N][M]);
 void creer_piece2(char matrice[N][M]);
 void creer_piece3(char matrice[N][M]);
 
-void creer_piece21(char matrice[A][B]);
-void creer_piece22(char matrice[A][B]);
-void creer_piece23(char matrice[A][B]);
-void creer_piece24(char matrice[A][B]);
 
-void creer_piece31(char matrice[C][D]);
-void creer_piece32(char matrice[C][D]);
-void creer_piece33(char matrice[C][D]);
-void creer_piece34(char matrice[C][D]);
-void creer_piece35(char matrice[C][D]);
+void creer_piece21(char matrice[O][P]);
+void creer_piece22(char matrice[O][P]);
+void creer_piece23(char matrice[O][P]);
+void creer_piece24(char matrice[O][P]);
 
-void affichage_map1(char matrice[N][M]);
-void affichage_map2(char matrice[A][B]);
-void affichage_map3(char matrice[C][D]);
+void creer_piece31(char matrice[M][Q]);
+void creer_piece32(char matrice[M][Q]);
+void creer_piece33(char matrice[M][Q]);
+void creer_piece34(char matrice[M][Q]);
+void creer_piece35(char matrice[M][Q]);
+*/
 
-void placement_tresors1(char matrice[N][M]);
-void placement_tresors2(char matrice[A][B]);
-void placement_tresors3(char matrice[C][D]);
+void affichage_map(char matrice[N][M]);
+void affichage_map2(char matrice[O][P]);
+void affichage_map3(char matrice[M][Q]);
+
+void placement_tresors(char matrice[N][M]);
+void placement_tresors2(char matrice[O][P]);
+void placement_tresors3(char matrice[M][Q]);
 
 int main(void);
