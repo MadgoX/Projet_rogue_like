ccaf1220648689414410eeacd50a038dad450913
MadgoX <36188123+MadgoX@users.noreply.github.com>
2018-04-03 14:32:00 +0000

    Update tresors.c

--- tresors.c
+++ tresors.c
@@ -1,72 +1,99 @@
+/*!
+*
+*	\file tresors.c
+*	\brief Fichier contenant l'implementation des tresors dans la matrice
+*	\author Lucien BOUYEURE, Romane BUON, Yann PAULMERY
+*	\version 3
+*
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
 #include "rogue_like.h"
 
-//Trouve la coordonnee en x du tresor
-int tresorx(int longueur) {
-	srand(time(NULL));
-	int i=0;
-	i=rand()%(longueur-2);
-	i++;
-	return i;
-}
 
-//Trouve la coordonnee en y du tresor
-int tresory(int largeur) {
-	srand(time(NULL));
-	int i=0;
-	i=rand()%(largeur-2);
-	i++;
-	return i;
-}
+/*!
+*	
+*	\fn placement_tresor1(char matrice[N][M],piece_t piece)
+*	\brief Fonction de placement de tresors dans le niveau 1
+*	\param matrice[N][M] Matrice principale du jeu
+*	\param piece Permet d'obtenir les valeurs de x_haut et y_gauche pour la piece13
+*
+*/
 
-/*Ces fonctions cherchent aleatoirement un endroit pour y placer le tresor, c'est a dire une case
-vide sans obstacle, representant l'interieur d'une piece*/
-
-//Pour le premier etage
-void placement_tresors1(char matrice[N][M]) {
-	int tresx=tresorx(N);
-	int tresy=tresory(M);
-	if(matrice[tresx][tresy]==' ')
-		matrice[tresx][tresy]='o';
-	else {
-		while(matrice[tresx][tresy]!=' ') {
-			tresx=tresorx(N);
-			tresy=tresory(M);
+//NIVEAU 1
+void placement_tresor1(char matrice[N][M], piece_t piece) {
+	int tresors[N][M]={0};	//On cree une matrice de int que l'on remplira soit de 0 soit d'une valeur k qui s'incrementera a chaque ajout
+	int k=1, l=0, i, j;		//Ainsi cette fonction permettra de creer de l'aleatoire de facon optimisee
+	for (i=piece.x_haut; i<N; i++) {	//On place le tresor dans la derniere piece de l'etage
+		for (j=piece.y_gauche; j<M; j++) {
+			if(matrice[i][j]==' ') {
+				tresors[i][j]=k;		//On place la valeur numerique a la case et on incremente k
+				k++;
+			}
 		}
-		matrice[tresx][tresy]='o';
 	}
+	l=rand_a_b(1, k);		//On recherche la case contenant le nombre aleatoire et on y place le tresor
+	for(i=0; i<N; i++)
+		for(j=0; j<M; j++)
+			if(tresors[i][j]==l)
+				matrice[i][j]='o';
 }
 
-//Pour le deuxieme etage
-void placement_tresors2(char matrice[O][P]) {
-	int tresx=tresorx(O);
-	int tresy=tresory(P);
-	if(matrice[tresx][tresy]==' ')
-		matrice[tresx][tresy]='o';
-	else {
-		while(matrice[tresx][tresy]!=' ') {
-			tresx=tresorx(O);
-			tresy=tresory(P);
+//Les deux autres fonction sont identiques a la premiere, a la difference des tailles de matrice
+
+/*!
+*	
+*	\fn placement_tresor2(char matrice[O][P],piece_t piece)
+*	\brief Fonction de placement de tresors dans le niveau 2
+*	\param matrice[N][M] Matrice principale du jeu
+*	\param piece Permet d'obtenir les valeurs de x_haut et y_gauche pour la piece24
+*
+*/
+
+//NIVEAU 2
+void placement_tresor2(char matrice[O][P], piece_t piece) {
+	int tresors[O][P]={0};
+	int k=1, l=0, i, j;
+	for (i=piece.x_haut; i<O; i++) {
+		for (j=piece.y_gauche; j<P; j++) {
+			if(matrice[i][j]==' ') {
+				tresors[i][j]=k;
+				k++;
+			}
 		}
-		matrice[tresx][tresy]='o';
 	}
+	l=rand_a_b(1, k);
+	for(i=0; i<O; i++)
+		for(j=0; j<P; j++)
+			if(tresors[i][j]==l)
+				matrice[i][j]='o';
 }
 
-//Pour le troisieme etage
-void placement_tresors3(char matrice[M][Q]) {
-	int tresx=tresorx(M);
-	int tresy=tresory(Q);
-	if(matrice[tresx][tresy]==' ')
-		matrice[tresx][tresy]='o';
-	else {
-		while(matrice[tresx][tresy]!=' ') {
-			tresx=tresorx(M);
-			tresy=tresory(Q);
+/*!
+*	
+*	\fn placement_tresor3(char matrice[M][Q],piece_t piece)
+*	\brief Fonction de placement de tresors dans le niveau 3
+*	\param matrice[M][Q] Matrice principale du jeu
+*	\param piece Permet d'obtenir les valeurs de x_haut et y_gauche pour la piece35
+*
+*/
+
+//NIVEAU 3
+void placement_tresor3(char matrice[M][Q], piece_t piece) {
+	int tresors[M][Q]={0};
+	int k=1, l=0, i, j;
+	for (i=piece.x_haut; i<M; i++) {
+		for (j=piece.y_gauche; j<Q; j++) {
+			if(matrice[i][j]==' ') {
+				tresors[i][j]=k;
+				k++;
+			}
 		}
-		matrice[tresx][tresy]='o';
 	}
+	l=rand_a_b(1, k);
+	for(i=0; i<M; i++)
+		for(j=0; j<Q; j++)
+			if(tresors[i][j]==l)
+				matrice[i][j]='o';
 }
-
-//La seule difference entre ces trois fonctions est, comme partout, la taille de la matrice.
