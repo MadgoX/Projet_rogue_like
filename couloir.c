ab92fa3e6c05378245a837feb02f397ac573adab
Yan72 <36165247+Yan72@users.noreply.github.com>
2018-04-05 17:24:45 +0200

    Add files via upload

--- couloir.c
+++ couloir.c
@@ -3,7 +3,7 @@
 *	\file main.c
 *	\brief Fonction main du projet
 *	\author Lucien BOUYEURE, Romane BUON, Yann PAULMERY
-*	\version 0.1
+*	\version 3.0
 *
 */
 
@@ -12,37 +12,12 @@
 #include<time.h>
 #include "rogue_like.h"
 
-
-//niveau 1 : 30/50
-//niveau 2 : 50/60
-//niveau 3 : 50/120
-
-/**
+/*!
+*	
+*	\fn remplir_matrice_compt1(int compt[N][M])
+*	\brief Fonction permettant de remplir la matrice compt pour le niveau 1 avec des 0 dans un premier temps.
+*	\param compt[N][M] Matrice remplie de nombres qui va permettre de générer les couloirs.
 *
-*	\struct piece_s
-*	\brief Structure contenant toutes les informations importantes concernant chacunes des pièces
-*
-*/
-/*typedef struct piece_s{
-	int x_haut;
-	int y_gauche;
-	int x_porte1;
-	int y_porte1;
-	char position_p1;			//vaut h (si la porte est sur le coté haut ou bas de la piece) ou v (si la porte est sur le coté droit ou gauche de la piece)
-	int x_porte2;
-	int y_porte2;
-	char position_p2;			//vaut h (si la porte est sur le coté haut ou bas de la piece) ou v (si la porte est sur le coté droit ou gauche de la piece)
-} piece_t;
-
-piece_t piece11, piece12, piece13;						//Pieces 1, 2 et 3 du niveau 1
-piece_t piece21, piece22, piece23, piece24;				//Pieces 1, 2, 3 et 4 du niveau 2
-piece_t piece31, piece32, piece33, piece34, piece35;	//Pieces 1, 2, 3, 4 et 5 du niveau 3
-
-int rand_a_b(int a, int b){
-
-    return rand()%(b-a) +a;
-
-}
 */
 static
 void remplir_matrice_compt1(int compt[N][M]){
@@ -52,6 +27,14 @@ void remplir_matrice_compt1(int compt[N][M]){
 	}
 }
 
+/*!
+*	
+*	\fn remplir_matrice_compt2(int compt[O][P])
+*	\brief Fonction permettant de remplir la matrice compt pour le niveau 2 avec des 0 dans un premier temps.
+*	\param compt[O][P] Matrice remplie de nombres qui va permettre de générer les couloirs.
+*
+*/
+
 static
 void remplir_matrice_compt2(int compt[O][P]){
 	for(int i = 0; i < O; i++){
@@ -60,6 +43,14 @@ void remplir_matrice_compt2(int compt[O][P]){
 	}
 }
 
+/*!
+*	
+*	\fn remplir_matrice_compt3(int compt[M][Q])
+*	\brief Fonction permettant de remplir la matrice compt pour le niveau 3 avec des 0 dans un premier temps.
+*	\param compt[M][Q] Matrice remplie de nombres qui va permettre de générer les couloirs.
+*
+*/
+
 static
 void remplir_matrice_compt3(int compt[M][Q]){
 	for(int i = 0; i < M; i++){
@@ -67,77 +58,19 @@ void remplir_matrice_compt3(int compt[M][Q]){
 			compt[i][j] = 0;
 	}
 }
-/*
-static
-void afficher_matrice_int(int matrice[M][Q]){
-	for(int i = 0; i<N; i++){
-		for(int j = 0; j<M; j++){
-			printf("%d ", matrice[i][j]);
-		}
-		printf("\n");
-	}
-}
 
-static
-void afficher_matrice(char matrice[M][Q]){
-	for(int i = 0; i<O; i++){
-		for(int j = 0; j<P; j++){
-			printf("%c", matrice[i][j]);
-		}
-		printf("\n");
-	}
-}
-
-void creer_map3(char matrice[M][Q])
-{
-
-	int i, j;
-	
-	for(i=0; i<M; i++){
-		for(j=0; j<Q; j++){
-			matrice[i][j]='.';
-		}
-	}
-
-	i=0;
-	for(j=1; j<Q-1; j++){
-		matrice[i][j]='=';
-	}
-	
-	j=0;
-	for(i=1; i<M-1; i++){
-		matrice[i][j]='#';
-	}
-
-	j=Q-1;
-	for(i=1; i<M-1; i++){
-		matrice[i][j]='#';
-	}
-
-	i=M-1;
-	for(j=1; j<Q-1; j++){
-		matrice[i][j]='=';
-	}
-	
-	i=0;
-	j=0;
-	matrice[i][j]='+';
-
-	i=M-1;
-	j=Q-1;
-	matrice[i][j]='+';
-	
-	i=M-1;
-	j=0;
-	matrice[i][j]='+';
-
-	i=0;
-	j=Q-1;
-	matrice[i][j]='+';
-
-}
+/*!
+*	
+*	\fn dijkstra1(int compt[N][M], piece_t piece1, piece_t piece2, int x, int y, int val)
+*	\brief Fonction récursive permettant de numéroter dans la matrice compt chaque case entre les deux portes à relier dans le premier niveau.
+*	\param compt[N][M] Matrice remplie de nombres qui va permettre de générer les couloirs.
+*	\pram piece1 Structure contenant les coordonnées de la base de la pièce contenant la première porte ainsi que les coordonnées de celle-ci.
+*	\param piece2 Structure contenant les coordonnées de la base de la pièce contenant la deuxième porte ainsi que les coordonnées de celle-ci.
+*	\param x Coordonnée verticale de la case courante observée par l'algorithme (est initialisée à 0 au premier appel de la fonction).
+*	\param y Coordonnée horizontale de la case courante observée par l'algorithme (est initialisée à 0 au premier appel de la fonction).
+*	\param val Valeur courante de la case observée par l'algorithme (est initialisée à 0 au premier appel de la fonction).
+*
 */
-
 
 void dijkstra1(int compt[N][M], piece_t piece1, piece_t piece2, int x, int y, int val)  //x, y et val doivent être initialisés à 0 lors du premier appel de la fonction, leurs valeurs sera modifée dans la fonction suite à la récursivité.
 {	
@@ -291,6 +224,19 @@ void dijkstra1(int compt[N][M], piece_t piece1, piece_t piece2, int x, int y, in
 	}
 }
 
+/*!
+*	
+*	\fn dijkstra2(int compt[O][P], piece_t piece1, piece_t piece2, int x, int y, int val)
+*	\brief Fonction récursive permettant de numéroter dans la matrice compt chaque case entre les deux portes à relier dans le deuxième niveau.
+*	\param compt[O][P] Matrice remplie de nombres qui va permettre de générer les couloirs.
+*	\pram piece1 Structure contenant les coordonnées de la base de la pièce contenant la première porte ainsi que les coordonnées de celle-ci.
+*	\param piece2 Structure contenant les coordonnées de la base de la pièce contenant la deuxième porte ainsi que les coordonnées de celle-ci.
+*	\param x Coordonnée verticale de la case courante observée par l'algorithme (est initialisée à 0 au premier appel de la fonction).
+*	\param y Coordonnée horizontale de la case courante observée par l'algorithme (est initialisée à 0 au premier appel de la fonction).
+*	\param val Valeur courante de la case observée par l'algorithme (est initialisée à 0 au premier appel de la fonction).
+*
+*/
+
 void dijkstra2(int compt[O][P], piece_t piece1, piece_t piece2, int x, int y, int val)  //x, y et val doivent être initialisés à 0 lors du premier appel de la fonction, leurs valeurs sera modifée dans la fonction suite à la récursivité.
 {	
 	if (compt[x][y] == 0){
@@ -442,6 +388,19 @@ void dijkstra2(int compt[O][P], piece_t piece1, piece_t piece2, int x, int y, in
 	}
 }
 
+/*!
+*	
+*	\fn dijkstra3(int compt[M][Q], piece_t piece1, piece_t piece2, int x, int y, int val)
+*	\brief Fonction récursive permettant de numéroter dans la matrice compt chaque case entre les deux portes à relier dans le troisième niveau.
+*	\param compt[M][Q] Matrice remplie de nombres qui va permettre de générer les couloirs.
+*	\pram piece1 Structure contenant les coordonnées de la base de la pièce contenant la première porte ainsi que les coordonnées de celle-ci.
+*	\param piece2 Structure contenant les coordonnées de la base de la pièce contenant la deuxième porte ainsi que les coordonnées de celle-ci.
+*	\param x Coordonnée verticale de la case courante observée par l'algorithme (est initialisée à 0 au premier appel de la fonction).
+*	\param y Coordonnée horizontale de la case courante observée par l'algorithme (est initialisée à 0 au premier appel de la fonction).
+*	\param val Valeur courante de la case observée par l'algorithme (est initialisée à 0 au premier appel de la fonction).
+*
+*/
+
 void dijkstra3(int compt[M][Q], piece_t piece1, piece_t piece2, int x, int y, int val)  //x, y et val doivent être initialisés à 0 lors du premier appel de la fonction, leurs valeurs sera modifée dans la fonction suite à la récursivité.
 {	
 	if (compt[x][y] == 0){
@@ -592,6 +551,16 @@ void dijkstra3(int compt[M][Q], piece_t piece1, piece_t piece2, int x, int y, in
 		}
 	}
 }
+
+/*!
+*	
+*	\fn couloir1(char matrice[N][M], piece_t piece1, piece_t piece2)
+*	\brief Fonction permettant de modifier la matrice représentant la carte afin de placer aléatoirement les coulirs du premier niveau.
+*	\param matrice[N][M] Matrice représentant la carte.
+*	\pram piece1 Structure contenant les coordonnées de la base de la pièce contenant la première porte ainsi que les coordonnées de celle-ci.
+*	\param piece2 Structure contenant les coordonnées de la base de la pièce contenant la deuxième porte ainsi que les coordonnées de celle-ci.
+*
+*/
 
 void couloir1(char matrice[N][M], piece_t piece1, piece_t piece2)
 {	
@@ -760,7 +729,73 @@ void couloir1(char matrice[N][M], piece_t piece1, piece_t piece2)
 				}
 			}
 		}
-		else if (x1 > x2 && y1 < y2){
+		else if (x1 > x2 && y1 >= y2){
+			if (alea %2 == 0){
+				if (compt[x][y-1] == val + 1){
+					if (orient_prec == 'v'){
+						matrice[x][y+1] = '+';
+						matrice[x-1][y] = '+';
+						matrice[x-1][y+1] = '+';
+					}
+					else{
+						matrice[x-1][y] = '+';
+						matrice[x+1][y] = '+';
+					}
+					y--;
+					orient_prec = 'h';
+				}
+
+				else{												//cas où on arrive a la limite et que le nombre aléatoire ne permet pas de continuer
+					if (compt[x-1][y] == val + 1){
+						if (orient_prec == 'h'){
+							matrice[x+1][y] = '+';
+							matrice[x][y-1] = '+';
+							matrice[x+1][y-1] = '+';
+						}
+						else{
+							matrice[x][y-1] = '+';
+							matrice[x][y+1] = '+';
+						}
+						x--;
+					}
+					orient_prec = 'v';
+				}
+			}
+
+			else{
+				if (compt[x-1][y] == val + 1){
+					if (orient_prec == 'h'){
+						matrice[x+1][y] = '+';
+						matrice[x][y-1] = '+';
+						matrice[x+1][y-1] = '+';
+					}
+					else{
+						matrice[x][y-1] = '+';
+						matrice[x][y+1] = '+';
+					}
+					x--;
+					orient_prec = 'v';
+				}
+
+				else{												//cas où on arrive a la limite et que le nombre aléatoire ne permet pas de continuer
+					if (compt[x][y-1] == val + 1){
+						if (orient_prec == 'v'){
+							matrice[x][y+1] = '+';
+							matrice[x-1][y] = '+';
+							matrice[x-1][y-1] = '+';
+						}
+						else{
+							matrice[x-1][y] = '+';
+							matrice[x+1][y] = '+';
+						}
+						y--;
+					}
+					orient_prec = 'h';
+				}
+			}
+		}
+
+		else if (x1 > x2 && y1 <= y2){
 			if (alea %2 == 0){
 				if (compt[x][y+1] == val + 1){
 					if (orient_prec == 'v'){
@@ -897,6 +932,15 @@ void couloir1(char matrice[N][M], piece_t piece1, piece_t piece2)
 	}
 }
 
+/*!
+*	
+*	\fn couloir2(char matrice[O][P], piece_t piece1, piece_t piece2)
+*	\brief Fonction permettant de modifier la matrice représentant la carte afin de placer aléatoirement les couloirs du deuxième niveau.
+*	\param matrice[O][P] Matrice représentant la carte.
+*	\pram piece1 Structure contenant les coordonnées de la base de la pièce contenant la première porte ainsi que les coordonnées de celle-ci.
+*	\param piece2 Structure contenant les coordonnées de la base de la pièce contenant la deuxième porte ainsi que les coordonnées de celle-ci.
+*
+*/
 
 void couloir2(char matrice[O][P], piece_t piece1, piece_t piece2)
 {	
@@ -1068,7 +1112,73 @@ void couloir2(char matrice[O][P], piece_t piece1, piece_t piece2)
 				}
 			}
 		}
-		else if (x1 > x2 && y1 < y2){
+		else if (x1 > x2 && y1 >= y2){
+			if (alea %2 == 0){
+				if (compt[x][y-1] == val + 1){
+					if (orient_prec == 'v'){
+						matrice[x][y+1] = '+';
+						matrice[x-1][y] = '+';
+						matrice[x-1][y+1] = '+';
+					}
+					else{
+						matrice[x-1][y] = '+';
+						matrice[x+1][y] = '+';
+					}
+					y--;
+					orient_prec = 'h';
+				}
+
+				else{												//cas où on arrive a la limite et que le nombre aléatoire ne permet pas de continuer
+					if (compt[x-1][y] == val + 1){
+						if (orient_prec == 'h'){
+							matrice[x+1][y] = '+';
+							matrice[x][y-1] = '+';
+							matrice[x+1][y-1] = '+';
+						}
+						else{
+							matrice[x][y-1] = '+';
+							matrice[x][y+1] = '+';
+						}
+						x--;
+					}
+					orient_prec = 'v';
+				}
+			}
+
+			else{
+				if (compt[x-1][y] == val + 1){
+					if (orient_prec == 'h'){
+						matrice[x+1][y] = '+';
+						matrice[x][y-1] = '+';
+						matrice[x+1][y-1] = '+';
+					}
+					else{
+						matrice[x][y-1] = '+';
+						matrice[x][y+1] = '+';
+					}
+					x--;
+					orient_prec = 'v';
+				}
+
+				else{												//cas où on arrive a la limite et que le nombre aléatoire ne permet pas de continuer
+					if (compt[x][y-1] == val + 1){
+						if (orient_prec == 'v'){
+							matrice[x][y+1] = '+';
+							matrice[x-1][y] = '+';
+							matrice[x-1][y-1] = '+';
+						}
+						else{
+							matrice[x-1][y] = '+';
+							matrice[x+1][y] = '+';
+						}
+						y--;
+					}
+					orient_prec = 'h';
+				}
+			}
+		}
+
+		else if (x1 > x2 && y1 <= y2){
 			if (alea %2 == 0){
 				if (compt[x][y+1] == val + 1){
 					if (orient_prec == 'v'){
@@ -1205,6 +1315,15 @@ void couloir2(char matrice[O][P], piece_t piece1, piece_t piece2)
 	}
 }
 
+/*!
+*	
+*	\fn couloir3(char matrice[M][Q], piece_t piece1, piece_t piece2)
+*	\brief Fonction permettant de modifier la matrice représentant la carte afin de placer aléatoirement les couloirs du troisième niveau.
+*	\param matrice[M][Q] Matrice représentant la carte.
+*	\pram piece1 Structure contenant les coordonnées de la base de la pièce contenant la première porte ainsi que les coordonnées de celle-ci.
+*	\param piece2 Structure contenant les coordonnées de la base de la pièce contenant la deuxième porte ainsi que les coordonnées de celle-ci.
+*
+*/
 
 void couloir3(char matrice[M][Q], piece_t piece1, piece_t piece2)
 {	
@@ -1373,7 +1492,73 @@ void couloir3(char matrice[M][Q], piece_t piece1, piece_t piece2)
 				}
 			}
 		}
-		else if (x1 > x2 && y1 < y2){
+		else if (x1 > x2 && y1 >= y2){
+			if (alea %2 == 0){
+				if (compt[x][y-1] == val + 1){
+					if (orient_prec == 'v'){
+						matrice[x][y+1] = '+';
+						matrice[x-1][y] = '+';
+						matrice[x-1][y+1] = '+';
+					}
+					else{
+						matrice[x-1][y] = '+';
+						matrice[x+1][y] = '+';
+					}
+					y--;
+					orient_prec = 'h';
+				}
+
+				else{												//cas où on arrive a la limite et que le nombre aléatoire ne permet pas de continuer
+					if (compt[x-1][y] == val + 1){
+						if (orient_prec == 'h'){
+							matrice[x+1][y] = '+';
+							matrice[x][y-1] = '+';
+							matrice[x+1][y-1] = '+';
+						}
+						else{
+							matrice[x][y-1] = '+';
+							matrice[x][y+1] = '+';
+						}
+						x--;
+					}
+					orient_prec = 'v';
+				}
+			}
+
+			else{
+				if (compt[x-1][y] == val + 1){
+					if (orient_prec == 'h'){
+						matrice[x+1][y] = '+';
+						matrice[x][y-1] = '+';
+						matrice[x+1][y-1] = '+';
+					}
+					else{
+						matrice[x][y-1] = '+';
+						matrice[x][y+1] = '+';
+					}
+					x--;
+					orient_prec = 'v';
+				}
+
+				else{												//cas où on arrive a la limite et que le nombre aléatoire ne permet pas de continuer
+					if (compt[x][y-1] == val + 1){
+						if (orient_prec == 'v'){
+							matrice[x][y+1] = '+';
+							matrice[x-1][y] = '+';
+							matrice[x-1][y-1] = '+';
+						}
+						else{
+							matrice[x-1][y] = '+';
+							matrice[x+1][y] = '+';
+						}
+						y--;
+					}
+					orient_prec = 'h';
+				}
+			}
+		}
+
+		else if (x1 > x2 && y1 <= y2){
 			if (alea %2 == 0){
 				if (compt[x][y+1] == val + 1){
 					if (orient_prec == 'v'){
@@ -1509,32 +1694,3 @@ void couloir3(char matrice[M][Q], piece_t piece1, piece_t piece2)
 		val++;		
 	}
 }
-/*
-int main(){
-
-	piece_t piece1;
-	piece_t piece2;
-	
-	int x1 = piece1.x_porte2 = 3;
-	int y1= piece1.y_porte2 = 45;
-	char pos1 = piece1.position_p2 = 'h';
-
-	int x2 = piece2.x_porte1 = 25;
-	int y2 = piece2.y_porte1 = 9;
-	char pos2 = piece2.position_p1 = 'h';
-	int x, y, x_fin, y_fin;
-
-	srand(time(NULL));
-
-	char matrice[M][Q];
-
-
-	int compt[M][Q];
-	creer_map3(matrice);
-
-	couloir3(matrice, piece1, piece2);
-
-	afficher_matrice(matrice);
-	//afficher_matrice_int(compt);
-
-}*/
